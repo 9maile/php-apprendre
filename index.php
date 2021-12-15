a9399985e2173af0206f5c9a4ca86fe279dc04f4
lina <monmail.16424@gmail.com>
2021-12-15 20:59:46 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<!doctype html>
+<html lang="fr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+     
+    <title>LeanaF</title>
+  </head>
+  <body>
+      <?php include ("./partial/_navbar.php"); ?>
+      <div class="container">
+      <h1>Le site de Leana</h1>
+      </div>
+    
+
+   
+    <script src="/js/bootstrap.bundle.min.js" ></script>
+
+    
+  </body>
+</html>   
+
---
+++ partial/_navbar.php
@@ -0,0 +1,31 @@
+
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">LeaneF</font></font></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Basculer la navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarColor02">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link active" href="/"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Accueil
+            </font></font><span class="visually-hidden"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">(courant)</font></font></span>
+          </a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="/test.php"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Test</font></font></a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="/exercices.php"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Exo</font></font></a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="/notes.php"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">notes</font></font></a>
+        </li>
+       
+    </div>
+  </div>
+</nav>   
+</body>
+</html>
